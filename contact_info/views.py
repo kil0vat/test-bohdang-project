0bb76a70a206cb9e9e821cebd91dfa60cfc0ed22
Bohdan Gorovy <bohdangorovoy@gmail.com>
2014-11-08 18:22:32 +0200

    ticket:1 Создал представления для моделей приложения contact_info

--- contact_info/views.py
+++ contact_info/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from contact_info.models import ContactInfo
+from django.template import loader, RequestContext
+from django.http import HttpResponse 
 # Create your views here.
+
+def base(request):
+	contact_info_list = ContactInfo.objects.all()
+	template = loader.get_template('contacts_info/base.html')
+	context = RequestContext('', {
+		'contact_info_list': contact_info_list
+	})
+	return HttpResponse(template.render(context))
